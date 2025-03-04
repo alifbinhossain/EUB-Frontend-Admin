a9b89394c4bdc0e95e6a509fa55b2ee50c27543b
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-03-04 17:23:13 +0600

    feat: add process management section with CRUD functionality and table configuration

--- src/routes/private/procurement.tsx
+++ src/routes/private/procurement.tsx
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 import { IRoute } from '@/types';
 
 const Category = lazy(() => import('@/pages/procurement/category'));
+const Process = lazy(() => import('@/pages/procurement/process'));
 
 const procurementRoutes: IRoute[] = [
 	{
@@ -13,6 +14,23 @@ const procurementRoutes: IRoute[] = [
 				element: <Category />,
 				page_name: 'procurement__category',
 				actions: ['create', 'read', 'update', 'delete'],
+			},
+			{
+				name: 'Process',
+				path: '/procurement/process',
+				element: <Process />,
+				page_name: 'procurement__process',
+				actions: [
+					'create',
+					'read',
+					'delete',
+					'click_items',
+					'click_service',
+					'click_range_1',
+					'click_range_2',
+					'click_range_3',
+					'click_range_4',
+				],
 			},
 		],
 	},
