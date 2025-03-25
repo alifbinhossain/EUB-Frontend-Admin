c75b76341ad9d0540ca32e574916629b3d8d7a65
Shafayat <shafayat1777@gmail.com>
2025-03-25 12:48:29 +0600

    ci: :bug: re-arranged nav and removed service vendor and general notes page

--- src/pages/procurement/general-note/config/columns/columns.type.ts
+++
@@ -1,9 +0,0 @@
-// * General Note
-export type IGeneralNoteTableData = {
-	uuid: string;
-	service_uuid: string;
-	service_name: string;
-	description: string;
-	amount: number;
-	remarks: string;
-};
