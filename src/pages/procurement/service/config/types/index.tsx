a16edf1b2192d899fdbf4c315530a49f4bfa79fb
Shafayat <shafayat1777@gmail.com>
2025-04-07 10:39:43 +0600

    ci: :bug: Fixed some bug

--- src/pages/procurement/service/config/types/index.tsx
+++ src/pages/procurement/service/config/types/index.tsx
@@ -3,10 +3,3 @@ import { UseMutationResult } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import '../columns/columns.type';
-
-import { IItemWorkOrderTableData } from '../columns/columns.type';
-
-//* Visitor
-export interface IItemAddOrUpdateProps extends IDefaultAddOrUpdateProps {
-	updatedData?: IItemWorkOrderTableData | null;
-}
