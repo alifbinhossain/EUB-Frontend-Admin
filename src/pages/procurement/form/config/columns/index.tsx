663e62ae61eade80b7cad61f93d9c99b64b36b1d
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-04-10 14:34:14 +0600

    refactor: :recycle: Remove unused imports in form columns configuration

--- src/pages/procurement/form/config/columns/index.tsx
+++ src/pages/procurement/form/config/columns/index.tsx
@@ -1,10 +1,6 @@
 import { ColumnDef } from '@tanstack/react-table';
-import { Download } from 'lucide-react';
 
 import FilePreview from '@/components/others/file-preview';
-import { buttonVariants } from '@/components/ui/button';
-
-import { API_IMAGE_URL } from '@/lib/secret';
 
 import { IFormTableData } from './columns.type';
 
