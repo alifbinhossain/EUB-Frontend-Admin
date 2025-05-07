ebf505050e1a18fe33e412d6fd5c866007d9dd4a
Shafayat <shafayat1777@gmail.com>
2025-05-07 16:39:35 +0600

    ci: :sparkles: Requisition pdf fix

--- src/components/pdf/item-requstion/utils.tsx
+++ src/components/pdf/item-requstion/utils.tsx
@@ -1,6 +1,6 @@
 import { EUB_LOGO } from '@/assets/images/base64';
 
-import { DEFAULT_FONT_SIZE } from '../ui';
+import { DEFAULT_FONT_SIZE, xMargin } from '../ui';
 import { getEmptyColumn } from '../utils';
 
 const PAGE_HEADER_EMPTY_ROW: string[] = ['', '', '', ''];
@@ -48,35 +48,102 @@ const EMPTY_COLUMN: string[] = getEmptyColumn(4);
 
 export const getPageFooter = ({ currentPage, pageCount }: { currentPage: number; pageCount: number }) => {
 	return {
-		widths: ['*', '*', '*'],
+		headerRows: 1,
+		widths: ['*', '*'],
 		body: [
+			[{ text: 'Remarks', colSpan: 2 }, ''],
 			[
-				{
-					text: 'SNO',
-					alignment: 'center',
-					border: [false, true, false, false],
-				},
-				{
-					text: '',
-					alignment: 'center',
-					border: [false, false, false, false],
-				},
-				{
-					text: 'Managing Director',
-					alignment: 'center',
-					border: [false, true, false, false],
-				},
+				{ text: 'Requisitioner:\n\n\n\n\n', rowSpan: 5 },
+				{ text: 'P&I:\n\n\n\n\n', rowSpan: 5 },
 			],
+			[{ text: '' }, { text: '' }],
+			[{ text: '' }, { text: '' }],
+			[{ text: '' }, { text: '' }],
+			[{ text: '' }, { text: '' }],
 			[
 				{
-					colSpan: 3,
+					colSpan: 2,
 					text: `Page ${currentPage} of ${pageCount}`,
 					alignment: 'center',
 					border: [false, false, false, false],
 				},
 				'',
-				'',
 			],
 		],
 	};
+
+	// return [
+	// 	{
+	// 		table: {
+	// 			headerRows: 1,
+	// 			widths: ['*', 10, '*', 10, '*'],
+	// 			body: [
+	// 				[
+	// 					{
+	// 						text: ``,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 					{
+	// 						text: ``,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 					{
+	// 						text: ``,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 					{
+	// 						text: ``,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 					{
+	// 						text: ``,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 				],
+	// 				[
+	// 					{ text: 'Requested By', alignment: 'center', border: [false, true, false, false] },
+	// 					{ text: '', alignment: 'center', border: [false, false, false, false] },
+	// 					{ text: 'Dept. Head', alignment: 'center', border: [false, true, false, false] },
+	// 					{ text: '', alignment: 'center', border: [false, false, false, false] },
+	// 					{ text: 'P&I CODE', alignment: 'center', border: [false, true, false, false] },
+	// 				],
+	// 			],
+	// 		},
+	// 		margin: [xMargin, 2],
+	// 	},
+	// 	{ text: '\n' },
+	// 	{
+	// 		table: {
+	// 			headerRows: 1,
+	// 			widths: ['*', '*'],
+	// 			body: [
+	// 				[{ text: 'Remarks', colSpan: 2 }, ''],
+	// 				[
+	// 					{ text: 'Requisitioner:\n\n\n\n\n', rowSpan: 5 },
+	// 					{ text: 'P&I:\n\n\n\n\n', rowSpan: 5 },
+	// 				],
+	// 				[{ text: '' }, { text: '' }],
+	// 				[{ text: '' }, { text: '' }],
+	// 				[{ text: '' }, { text: '' }],
+	// 				[{ text: '' }, { text: '' }],
+	// 				[
+	// 					{
+	// 						colSpan: 2,
+	// 						text: `Page ${currentPage} of ${pageCount}`,
+	// 						alignment: 'center',
+	// 						border: [false, false, false, false],
+	// 					},
+	// 					'',
+	// 				],
+	// 			],
+	// 		},
+	// 		margin: [xMargin, 2],
+	// 		fontSize: DEFAULT_FONT_SIZE,
+	// 	},
+	// ];
 };
