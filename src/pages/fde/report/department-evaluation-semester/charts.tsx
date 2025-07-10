e3a1bbeeb889c3b8c95ea5245263a0a7cdb129a5
Shafayat <shafayat1777@gmail.com>
2025-07-10 19:35:22 +0600

    ci: :sparkles: Added Chard in Report: Department Evaluation Semester

--- src/pages/fde/report/department-evaluation-semester/charts.tsx
+++ src/pages/fde/report/department-evaluation-semester/charts.tsx
@@ -1,77 +1,67 @@
-// 'use client';
+'use client';
 
-// import { TrendingUp } from 'lucide-react';
-// import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+import { useMemo } from 'react';
+import { TrendingUp } from 'lucide-react';
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 
-// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-// import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 
-// export const description = 'A linear area chart';
+import { IReportDepartmentEvaluationSemesterTableData } from '../config/columns/columns.type';
 
-// const chartData = [
-// 	{ month: 'January', desktop: 186 },
-// 	{ month: 'February', desktop: 305 },
-// 	{ month: 'March', desktop: 237 },
-// 	{ month: 'April', desktop: 73 },
-// 	{ month: 'May', desktop: 209 },
-// 	{ month: 'June', desktop: 214 },
-// ];
+export const description = 'A linear area chart';
 
-// const chartConfig = {
-// 	desktop: {
-// 		label: 'Desktop',
-// 		color: 'var(--chart-1)',
-// 	},
-// } satisfies ChartConfig;
+const chartConfig = {
+	average: {
+		label: 'Average',
+		color: '#2563eb',
+	},
+} satisfies ChartConfig;
 
-// export function ChartAreaLinear() {
-// 	return (
-// 		<Card>
-// 			<CardHeader>
-// 				<CardTitle>Area Chart - Linear</CardTitle>
-// 				<CardDescription>Showing total visitors for the last 6 months</CardDescription>
-// 			</CardHeader>
-// 			<CardContent>
-// 				<ChartContainer config={chartConfig}>
-// 					<AreaChart
-// 						accessibilityLayer
-// 						data={chartData}
-// 						margin={{
-// 							left: 12,
-// 							right: 12,
-// 						}}
-// 					>
-// 						<CartesianGrid vertical={false} />
-// 						<XAxis
-// 							dataKey='month'
-// 							tickLine={false}
-// 							axisLine={false}
-// 							tickMargin={8}
-// 							tickFormatter={(value) => value.slice(0, 3)}
-// 						/>
-// 						<ChartTooltip cursor={false} content={<ChartTooltipContent indicator='dot' hideLabel />} />
-// 						<Area
-// 							dataKey='desktop'
-// 							type='linear'
-// 							fill='var(--color-desktop)'
-// 							fillOpacity={0.4}
-// 							stroke='var(--color-desktop)'
-// 						/>
-// 					</AreaChart>
-// 				</ChartContainer>
-// 			</CardContent>
-// 			<CardFooter>
-// 				<div className='flex w-full items-start gap-2 text-sm'>
-// 					<div className='grid gap-2'>
-// 						<div className='flex items-center gap-2 font-medium leading-none'>
-// 							Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
-// 						</div>
-// 						<div className='flex items-center gap-2 leading-none text-muted-foreground'>
-// 							January - June 2024
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</CardFooter>
-// 		</Card>
-// 	);
-// }
+export function ChartAreaLinear({ data }: { data: IReportDepartmentEvaluationSemesterTableData }) {
+	const transformedData = useMemo(() => {
+		return Object.entries(data).map(([key, value]) => {
+			// Transform the key (e.g., "spring_2019" to "Spring 2019")
+			const semester = key.split('_').join(' ').toUpperCase();
+			return { semester: semester, average: value };
+		});
+	}, [data]);
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Semester Wise Average - Linear</CardTitle>
+				<CardDescription>Showing semester wise average scenario</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ChartContainer config={chartConfig} className='h-40 w-full'>
+					<AreaChart accessibilityLayer data={transformedData} margin={{ left: 40, right: 40 }}>
+						<CartesianGrid vertical={true} />
+						<XAxis
+							dataKey='semester'
+							tickLine={false}
+							axisLine={false}
+							tickMargin={8}
+							tickFormatter={(value) => value}
+						/>{' '}
+						<YAxis tickLine={false} axisLine={false} tickMargin={8} domain={['dataMax', 'dataMax + 5']} />
+						<ChartTooltip cursor={false} content={<ChartTooltipContent indicator='dot' hideLabel />} />
+						<Area dataKey='average' type='linear' fill='#60a5fa' fillOpacity={0.3} stroke='#2563eb' />
+					</AreaChart>
+				</ChartContainer>
+			</CardContent>
+			<CardFooter>
+				<div className='flex w-full items-start gap-2 text-sm'>
+					<div className='grid gap-2'>
+						{/* <div className='flex items-center gap-2 font-medium leading-none'>
+							Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
+						</div> */}
+						<div className='flex items-center gap-2 leading-none text-muted-foreground'>
+							{transformedData[0]?.semester} - {transformedData[transformedData.length - 1]?.semester}
+						</div>
+					</div>
+				</div>
+			</CardFooter>
+		</Card>
+	);
+}
--- src/pages/fde/report/department-evaluation-semester/index.tsx
+++ src/pages/fde/report/department-evaluation-semester/index.tsx
@@ -8,14 +8,15 @@ import { useOtherDepartments } from '@/lib/common-queries/other';
 import { PageInfo } from '@/utils';
 
 import { departmentEvaluationSemesterColumns } from '../config/columns';
-import { IReportTeacherEvaluationTeacherTableData } from '../config/columns/columns.type';
+import { IReportDepartmentEvaluationSemesterTableData } from '../config/columns/columns.type';
 import { useFDEReportDepartmentEvaluationSemester } from '../config/query';
+import { ChartAreaLinear } from './charts';
 
 const Semester = () => {
 	const [department, setDepartment] = useState('');
 
 	const { data, isLoading, url, refetch } =
-		useFDEReportDepartmentEvaluationSemester<IReportTeacherEvaluationTeacherTableData[]>(department);
+		useFDEReportDepartmentEvaluationSemester<IReportDepartmentEvaluationSemesterTableData[]>(department);
 	const { data: departmentOptions } = useOtherDepartments<IFormSelectOption[]>();
 
 	const pageInfo = useMemo(
@@ -37,38 +38,42 @@ const Semester = () => {
 
 	return (
 		<PageProvider pageName={pageInfo.getTab()} pageTitle={pageInfo.getTabName()}>
-			<TableProvider
-				title={pageInfo.getTitle()}
-				columns={columns}
-				data={data ?? []}
-				isLoading={isLoading}
-				handleRefetch={refetch}
-				defaultVisibleColumns={{
-					remarks: false,
-					created_at: false,
-					updated_at: false,
-					created_by_name: false,
-					actions: false,
-				}}
-				toolbarOptions={['all', 'export-csv', 'export-pdf']}
-				otherToolBarComponents={
-					<div className='flex gap-4'>
-						<ReactSelect
-							placeholder='Select Department'
-							className='w-96'
-							options={departmentOptions || []}
-							value={departmentOptions?.find((option) => option.value === department)}
-							menuPortalTarget={document.body}
-							styles={{
-								menuPortal: (base) => ({ ...base, zIndex: 999 }),
-							}}
-							onChange={(e: any) => {
-								setDepartment(e?.value);
-							}}
-						/>
-					</div>
-				}
-			/>
+			<div className='flex flex-col gap-8'>
+				<TableProvider
+					title={pageInfo.getTitle()}
+					columns={columns}
+					data={data ?? []}
+					isLoading={isLoading}
+					handleRefetch={refetch}
+					defaultVisibleColumns={{
+						remarks: false,
+						created_at: false,
+						updated_at: false,
+						created_by_name: false,
+						actions: false,
+					}}
+					toolbarOptions={['all', 'export-csv', 'export-pdf']}
+					otherToolBarComponents={
+						<div className='flex gap-4'>
+							<ReactSelect
+								placeholder='Select Department'
+								className='w-96'
+								options={departmentOptions || []}
+								value={departmentOptions?.find((option) => option.value === department)}
+								menuPortalTarget={document.body}
+								styles={{
+									menuPortal: (base) => ({ ...base, zIndex: 999 }),
+								}}
+								onChange={(e: any) => {
+									setDepartment(e?.value);
+								}}
+							/>
+						</div>
+					}
+				/>
+
+				<ChartAreaLinear data={data?.[0] || {}} />
+			</div>
 		</PageProvider>
 	);
 };
