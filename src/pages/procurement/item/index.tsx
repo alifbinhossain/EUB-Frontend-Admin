7094fdd81110c17df651dc92bcc72141d0497e07
Shafayat <shafayat1777@gmail.com>
2025-07-21 19:58:08 +0600

    ci: :bug: Fixed store type access bug in Items page

--- src/pages/procurement/item/index.tsx
+++ src/pages/procurement/item/index.tsx
@@ -18,15 +18,19 @@ const Details = lazy(() => import('./details'));
 const DeleteModal = lazy(() => import('@core/modal/delete'));
 
 const getAccess = (pageAccess: string[]) => {
+	const types: string[] = [];
+
 	if (pageAccess.includes('show_maintenance')) {
-		return 'store_type=maintenance';
-	} else if (pageAccess.includes('show_general')) {
-		return 'store_type=general';
-	} else if (pageAccess.includes('show_it_store')) {
-		return 'store_type=it_store';
-	} else {
-		return '';
+		types.push('maintenance');
 	}
+	if (pageAccess.includes('show_general')) {
+		types.push('general');
+	}
+	if (pageAccess.includes('show_it_store')) {
+		types.push('it_store');
+	}
+
+	return types.length > 0 ? `store_type=${types.join(',')}` : '';
 };
 
 const Designation = () => {
@@ -36,6 +40,7 @@ const Designation = () => {
 	const { data, isLoading, url, deleteData, postData, updateData, refetch } = useItem<IItemTableData[]>(
 		getAccess(pageAccess)
 	);
+	console.log(getAccess(pageAccess));
 	const [itemUuid, setItemUuid] = useState<string | null>(null);
 
 	const pageInfo = useMemo(() => new PageInfo('Item', url, 'procurement__item'), [url]);
